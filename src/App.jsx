7483e85ce285d4bc60fbf8f136d9e2d16c26333d
Gil Even <gil@targetmedia-ads.com>
2026-03-29 10:33:14 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
+import { AuthProvider, useAuth } from '@/lib/AuthContext';
+import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
