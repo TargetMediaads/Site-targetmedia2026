3052b8d403e8b7b21497f66749bec41462587e86
TargetMediaads <87409690+TargetMediaads@users.noreply.github.com>
2026-03-31 23:09:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require('express');
+const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,10 +13,8 @@ if (API_URL) {
   }));
 }
 
-// Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
